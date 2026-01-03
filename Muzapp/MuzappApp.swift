88514b9569f29af0f76faf1c5c9834841121567d
William <wbbarrios@gmail.com>
2026-01-02 20:43:58 -0800

    Initial Commit

---
+++ Muzapp/MuzappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuzappApp.swift
+//  Muzapp
+//
+//  Created by William Barrios on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MuzappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
